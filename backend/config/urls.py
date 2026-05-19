fb57bc7764c8ac1fd7b49a366fe1a764d62cf99f
gesa <gesanei87@gmail.com>
2026-05-19 23:57:22 +0000

    test cicd: 5!

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -24,7 +24,7 @@ from rest_framework.response import Response
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def check_health(request):
-    return Response({'status': 'OK!!!'})
+    return Response({'status': 'OK!!!!!'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
